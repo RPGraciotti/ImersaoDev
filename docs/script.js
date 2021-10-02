97247bc08ada2d0b3e1617605cf9eef1efea6351
Rodolfo P Graciotti <rodolfo.graciotti@gmail.com>
2021-10-01 21:19:51 -0300

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,7 @@
+function mudaTema() {
+	document.body.classList.toggle("dark");
+}
+
+// Desafios
+// Terminar todo o design que está no figma
+// Adicionar suas redes sociais de forma que quem clique consia abrí-las
